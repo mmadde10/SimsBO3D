668867c3501a330aced0ee533acd8a4a3742e70b
Mark Madden <mmadden1495@gmail.com>
2019-03-03 19:01:56 -0600

    Initial Commit

---
+++ SimsBO3DTests/SimsBO3DTests.swift
@@ -0,0 +1,34 @@
+//
+//  SimsBO3DTests.swift
+//  SimsBO3DTests
+//
+//  Created by Mark Madden on 3/3/19.
+//  Copyright © 2019 Mark Madden. All rights reserved.
+//
+
+import XCTest
+@testable import SimsBO3D
+
+class SimsBO3DTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
